55ff411d56c82e518ac575b7f1a803707806b90b
Amirali Dabiri <a.dmaram2023@gmail.com>
2025-12-29 22:40:39 +0330

    gradient

--- src/GradientText.jsx
+++ src/GradientText.jsx
@@ -6,7 +6,7 @@ export default function GradientText({
   children,
   className = '',
   // لیست اصلی رنگ‌ها بدون تکرار دستی
-  colors = ["#DF9339", "#D1765C", "#A010D6","#D1765C", "#DF9339", "#F3BC08", "#DF9339","#D1765C"],
+  colors = ["#DF9339", "#D1765C", "#A010D6","#D1765C", "#DF9339", "#F3BC08"],
   animationSpeed = 8,
   showBorder = false,
   pauseOnHover = false,
